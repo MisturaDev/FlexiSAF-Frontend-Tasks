edc8550be0efb1fb14bc35b9890a113dd7ce065b
TurahRae <Turahrae@gmail.com>
2026-02-19 23:32:39 +0100

    Fix chalk error by switching to ES module import

---
+++ Flexisaf_Task5/index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.green("Hello from Node.js"));
+console.log(chalk.blue("This is my Task 5 implementation"));
+console.log(chalk.yellow("Learning npm is fun!"));
